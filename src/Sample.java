6e0c88a99ab9e8b806199d4f0eb018aacc685419
sivagane <sivag918@gmail.com>
2020-01-28 12:43:58 +0530

    Siva updated

--- src/Sample.java
+++ src/Sample.java
@@ -2,5 +2,7 @@
 public class Sample {
 public static void main(String[] args) {
 	System.out.println("Hello World !");
+	System.out.println("I am  siva");
+	System.out.println("I like typing");
 }
 }
